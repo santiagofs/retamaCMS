8d9e8e58807e50a7ba4e3e3737c4d004975b4aa1
Aureliano Far Suau <aurelianofarsuau@Aurelianos-MacBook-Pro.local>
2018-02-15 18:11:05 -0300

    JWT

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\User;
+use JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Validator;
+use DB, Hash, Mail, Illuminate\Support\Facades\Password;
+
+class AuthController extends Controller
+{
+    /**
+     * API Register
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function register(Request $request)
+    {
+        $credentials = $request->only('name', 'email', 'password');
+
+        $rules = [
+            'name' => 'required|max:255|unique:users',
+            'email' => 'required|email|max:255|unique:users',
+            'password' => 'required|max:255',
+        ];
+
+        $validator = Validator::make($credentials, $rules);
+
+        if($validator->fails()) {
+            return response()->json(['success'=> false, 'error'=> $validator->messages()]);
+        }
+
+        $name = $request->name;
+        $email = $request->email;
+        $password = $request->password;
+
+        User::create(['name' => $name, 'email' => $email, 'password' => Hash::make($password)]);
+
+        // return $this->login($request);
+        return response()->json(['success' => true, 'message'=> "You have successfully register."]);
+    }
+
+    /**
+     * API Login, on success return JWT Auth token
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        $rules = [
+            'email' => 'required|email',
+            'password' => 'required',
+        ];
+
+        $validator = Validator::make($credentials, $rules);
+
+        if($validator->fails()) {
+            return response()->json(['success'=> false, 'error'=> $validator->messages()]);
+        }
+
+        try {
+            // attempt to verify the credentials and create a token for the user
+            if (!$token = JWTAuth::attempt($credentials)) {
+                return response()->json(['success' => false, 'error' => 'We cant find an account with this credentials.'], 401);
+            }
+
+        } catch (JWTException $e) {
+            // something went wrong whilst attempting to encode the token
+            return response()->json(['success' => false, 'error' => 'Failed to login, please try again.'], 500);
+        }
+
+        // all good so return the token
+        return response()->json(['success' => true, 'data'=> [ 'token' => $token ]]);
+    }
+
+    /**
+     * Log out
+     * Invalidate the token, so user cannot use it anymore
+     * They have to relogin to get a new token
+     *
+     * @param Request $request
+     */
+    public function logout(Request $request)
+    {
+        $this->validate($request, ['token' => 'required']);
+
+        try {
+            JWTAuth::invalidate($request->input('token'));
+            return response()->json(['success' => true, 'message'=> "You have successfully logged out."]);
+
+        } catch (JWTException $e) {
+            // something went wrong whilst attempting to encode the token
+            return response()->json(['success' => false, 'error' => 'Failed to logout, please try again.'], 500);
+
+        }
+    }
+}
